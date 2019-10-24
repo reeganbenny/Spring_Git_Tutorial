f0dba787d5b0f82718e4df0f7f187784da2762f8
Reegan Benny <reeganbenny@gmail.com>
2019-10-25 03:16:20 +0530

    Add Spring properties to the project and change the class TrachCoach to TrackCoach

--- Spring_Demo_One/src/com/luv2code/springdemo/TrackCoach.java
+++ Spring_Demo_One/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,10 +1,10 @@
 package com.luv2code.springdemo;
 
-public class TrachCoach implements Coach {
+public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		return "Run hardly 5km. ";
+		return "Run a hard 5k. ";
 	}
 
 }
